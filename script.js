eed526d5b724057346d515029691ba2910303599
chenpetro <chenpetro@gmail.com>
2024-04-26 17:39:16 +0200

    avatar site

---
+++ script.js
@@ -0,0 +1,5 @@
+// const btn = document.getElementById('btn')
+// const testbutton = document.getElementById('testbutton')
+// btn.addEventListener('click', function(){
+//     testbutton.classList.toggle('show')
+// })
